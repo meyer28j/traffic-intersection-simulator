ebea02f26153af002f5e3dbbe08110e909df4ab1
Jacob Meyer <meyer28j@uregina.ca>
2024-12-03 12:08:05 -0600

    Update GPIO functions; add clearAll()

--- Core/Inc/gpio.h
+++ Core/Inc/gpio.h
@@ -10,9 +10,51 @@
 
 #include "stm32f1xx_hal.h"
 
+/*
+ * Values taken from wiring schematic, align pin
+ * with connected traffic light purpose
+ */
+// north/south uses GPIOA
+typedef enum
+{
+	LED_WALK = GPIO_PIN_6,
+	LED_WAIT = GPIO_PIN_7,
+	LED_TURNING = GPIO_PIN_8,
+	LED_GREEN = GPIO_PIN_9,
+	LED_YELLOW = GPIO_PIN_10,
+	LED_RED = GPIO_PIN_1
+} ledPinNS;
+
+// east/west uses GPIOB
+/*
+typedef enum
+{
+	LED_WALK = GPIO_PIN_,
+	LED_WAIT = GPIO_PIN_,
+	LED_TURNING = GPIO_PIN_,
+	LED_GREEN = GPIO_PIN_,
+	LED_YELLOW = GPIO_PIN_,
+	LED_RED = GPIO_PIN_
+} ledPinEW;
+*/
+
+/*
+const ledPinEW pinsEW[] =
+{
+		LED_WALK,
+		LED_WAIT,
+		LED_TURNING,
+		LED_GREEN,
+		LED_YELLOW,
+		LED_RED,
+};
+*/
 // LED control functions
-void LED_toggle(GPIO_Typedef * port, uint16_t pin);
-void LED_on(GPIO_Typedef * port, uint16_t pin);
-void LED_off(GPIO_Typedef * port, uint16_t pin);
+void ledToggle(GPIO_TypeDef * port, uint16_t pin);
+void ledOn(GPIO_TypeDef * port, uint16_t pin);
+void ledOff(GPIO_TypeDef * port, uint16_t pin);
+
+// disable all outputs
+void clearAll();
 
 #endif /* GPIO_H_ */
--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -7,23 +7,42 @@
 
 #include "gpio.h"
 
+// define all LEDs for clearAll function
+static const ledPinNS PINS_NS[] =
+{
+	LED_WALK,
+	LED_WAIT,
+	LED_TURNING,
+	LED_GREEN,
+	LED_YELLOW,
+	LED_RED,
+};
+
 /*
  * LED control functions
  */
-void LED_toggle(GPIO_Typedef * port, uint16_t pin)
+void ledToggle(GPIO_TypeDef * port, uint16_t pin)
 {
 	HAL_GPIO_TogglePin(port, pin); // toggle light
 	return;
 }
 
-void LED_off(GPIO_Typedef * port, uint16_t pin)
+void ledOff(GPIO_TypeDef * port, uint16_t pin)
 {
 	HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
 	return;
 }
 
-void LED_on(GPIO_Typedef * port, uint16_t pin)
+void ledOn(GPIO_TypeDef * port, uint16_t pin)
 {
 	HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
 	return;
+}
+
+void clearAll()
+{
+	for (uint16_t pin = 0; pin < sizeof(PINS_NS) / sizeof(PINS_NS[0]); pin++)
+	{
+		ledOff(GPIOA, PINS_NS[pin]);
+	}
 }
